0566ce0c90f5b7f7090bf9f15ca6deab4b11e90e
Daniel Barros <barros.daniel@outlook.com>
2019-03-27 17:49:55 +0800

    feature: Add 'Add Task' event script

---
+++ app.js
@@ -0,0 +1,49 @@
+// Define UI variables
+const form = document.querySelector("#task-form");
+const taskList = document.querySelector(".collection");
+const clearBtn = document.querySelector(".clear-tasks");
+const filter = document.querySelector("#filter");
+const taskInput = document.querySelector("#task");
+
+// Load All Event Listeners
+loadEventListeners();
+
+// Load All Event Listeners
+function loadEventListeners() {
+  // Add Task Event
+  form.addEventListener("submit", addTask);
+  // Remove task event
+  taskList.addEventListener("click", removeTask);
+}
+
+// Add Task
+function addTask(e) {
+  if (taskInput.value === "") {
+    alert("Enter a task");
+  }
+
+  // Create li element
+  const li = document.createElement("li");
+  // Add Materialize li class
+  li.className = "collection-item";
+  // Create Text node and append to li
+  const textNode = document.createTextNode(taskInput.value);
+  li.appendChild(textNode);
+
+  // Create new link element
+  const link = document.createElement("a");
+  // Add class to link
+  link.className = "delete-item secondary-content";
+  // Add icon
+  link.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
+  // Append link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear input
+  taskInput.value = "";
+
+  e.preventDefault();
+}
